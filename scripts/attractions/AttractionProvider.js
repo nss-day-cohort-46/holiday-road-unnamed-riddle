c1ff9cfe62199e4cb2fede23a9f4038884df6840
CheoR <cheo.roman+github@gmail.com>
2021-02-04 14:28:17 -0600

    Render details for selected attraction  below select drow-down

--- scripts/attractions/AttractionProvider.js
+++ scripts/attractions/AttractionProvider.js
@@ -1,51 +1,70 @@
+import { Attraction } from "./Attraction.js"
 /*
  Bizarrieres
 */
 
 let _attractions = []
+const _url = "http://holidayroad.nss.team/bizarreries"
+const eventHub = document.querySelector(".mainContainer")
+
 
 export const useAttractions = () => _attractions.slice()
 
+
 export const getAttractions = () => {
- const url = "http://holidayroad.nss.team/bizarreries"
-  return fetch(url)
+  return fetch(_url)
   .then(response => response.json())
   .then(parsedAttractions => {
    _attractions = parsedAttractions
   })
 } // getAttractions
 
-const _render = (attractionCollection) => {
- const targetElement = document.querySelector(".mainContainer")
- 
- let attractions = attractionCollection.map((attraction) => {
-   return `<option value=${attraction.id}>${attraction.name}</option>`    
-  }).join("")
-
-  targetElement.innerHTML += `
-    <label for="attraction-select">Choose attraction:</label>
-    <select name="attractions" id="attraction-select">
-      <option value="0">--Please select an attractoin--</option>
-      ${attractions}
-    </select>
-  `
-} // _render
 
 export const LoadAttractions = () => {
  getAttractions()
-  .then(() => {
+  .then(() => { 
    const parsedAttractions = useAttractions()
    _render(parsedAttractions)
   })
 } // LoadAttractions
 
 
-const eventHub = document.querySelector(".mainContainer")
+const _render = (attractionCollection) => {
+ const targetElement = document.querySelector(".attractionSelect")
+ 
+ let attractions = attractionCollection.map((attraction) => `<option value=${attraction.id}>${attraction.name}</option>`).join("")
 
-eventHub.addEventListener ("change", changeEvent => {
-  if(changeEvent.target.id === "attraction-select") {
-    const option = parseInt(changeEvent.target.value)
+  targetElement.innerHTML += `
+    <label for="attraction-select">Choose attraction:</label>
+    <select name="attractions" id="attraction-select" class="selectAttraction">
+      <option value="0" class="test">--Please select an attractoin--</option>
+      ${attractions}
+    </select>
+    <div class="attractionPreview"></div>
+  `
+
+  // only fire after options loaded
+  eventHub.addEventListener("change", changeEvent => {
+
+    if(changeEvent.target.id === "attraction-select") {
+      const customEvent = new CustomEvent("attractionsLoaded", {
+        detail: {
+          attractionsLoaded: true,
+          attractionId: parseInt(changeEvent.target.value)
+        }
+      })
+      eventHub.dispatchEvent(customEvent)
+    } // if
+  }) // eventHub
+} // _render
+
+
+eventHub.addEventListener ("attractionsLoaded", changeEvent => {
+  if(changeEvent.detail.attractionsLoaded) {
+    const attractionId = changeEvent.detail.attractionId
+    const targetElement = document.querySelector(".attractionPreview")
+    const attraction = useAttractions().find((attraction) => attraction.id === attractionId)
     
-    
+    targetElement.innerHTML = `${Attraction(attraction)}`
   } 
 })
