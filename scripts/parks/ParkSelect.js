dc397c5412d3d466a898b9e4ffa6fd9651985a63
Dan Ross <daniellross@gmail.com>
2021-02-04 11:32:04 -0600

    adjusted class to match Sams for appending to dom

--- scripts/parks/ParkSelect.js
+++ scripts/parks/ParkSelect.js
@@ -1,7 +1,7 @@
 import { getParks, useParks } from './ParkProvider.js'
 
 const eventHub = document.querySelector('.mainContainer')
-const contentTarget = document.querySelector('.mainContainer')
+const contentTarget = document.querySelector('.parkSelect')
 
 export const parkSelect = () => {
     getParks()
@@ -13,8 +13,15 @@ export const parkSelect = () => {
 
 const render = (parksArray) => {
     contentTarget.innerHTML = `
-    <select name="class="dropDown" id="parkSelect">
-    <option value="0">Select a Park</option>
+    <label for="parkSelect">National Parks</label>
+    <select name="parkSelect" class="dropDown" id="parkSelect">
+    <option value="0">Select a Park...</option>
     ${parksArray.map(parkObject => `<option value="${parkObject.id}">${parkObject.fullName}</option>`)} 
 </select>`
 }
+
+eventHub.addEventListener("change", changeEvent => {
+    if (changeEvent.target.id === "parkSelect") {
+        const parkSelected = new CustomEvent
+    }
+})
