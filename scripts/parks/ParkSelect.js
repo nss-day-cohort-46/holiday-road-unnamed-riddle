a67a42fbc1cdfb39f5a811581ad59f7c0c14ddb1
Dan Ross <daniellross@gmail.com>
2021-02-04 10:57:02 -0600

    all parks now apear in dropdown

--- scripts/parks/ParkSelect.js
+++ scripts/parks/ParkSelect.js
@@ -1,7 +1,7 @@
 import { getParks, useParks } from './ParkProvider.js'
 
-const eventHub = document.querySelector('.contentContainer')
-const contentTarget = document.querySelector('.contentContainer')
+const eventHub = document.querySelector('.mainContainer')
+const contentTarget = document.querySelector('.mainContainer')
 
 export const parkSelect = () => {
     getParks()
@@ -12,5 +12,9 @@ export const parkSelect = () => {
 }
 
 const render = (parksArray) => {
-    contentTarget.innerHTML = 
+    contentTarget.innerHTML = `
+    <select name="class="dropDown" id="parkSelect">
+    <option value="0">Select a Park</option>
+    ${parksArray.map(parkObject => `<option value="${parkObject.id}">${parkObject.fullName}</option>`)} 
+</select>`
 }
