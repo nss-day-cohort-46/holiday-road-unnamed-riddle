4657f14b56b3f18a76d4d60d650249666011e031
daprieu <daprieu@gmail.com>
2021-02-05 14:31:41 -0600

    add weather Icons for weath display

--- scripts/weather/Weather.js
+++ scripts/weather/Weather.js
@@ -1,12 +1,12 @@
 
 export const Weather = (weatherObj) => {
+
     return `
     <div class="weather">
         <p class="day">Day: ${new Date(weatherObj.dt * 1000).toLocaleDateString('en-US')}</P>
-        <p class="min">Min: ${weatherObj.temp.min}</P>
-        <p class="max">Max: ${weatherObj.temp.max}</P>
-        <p class="weather">Weather: ${weatherObj.weather[0].main}</P>
-        <p class="weatherDetail">Detail: ${weatherObj.weather[0].description}</P>
+        <p class="min/max">Min: ${weatherObj.temp.min} / Max: ${weatherObj.temp.max}</P>
+        <p class="weatherDetail"> ${weatherObj.weather[0].description}</P>
+        <img src="http://openweathermap.org/img/w/${weatherObj.weather[0].icon}.png" alt=""
         </div>
         `
 }
