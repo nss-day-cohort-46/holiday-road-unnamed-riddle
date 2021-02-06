ea14bc4ee11b2bef901f68da5bd605e790828744
CheoR <cheo.roman+github@gmail.com>
2021-02-05 22:42:47 -0600

    Refactor attraction detail.

--- scripts/attractions/Attraction.js
+++ scripts/attractions/Attraction.js
@@ -1,5 +1,24 @@
 const eventHub = document.querySelector('.mainContainer')
 
+export const Attraction = (attraction) => {
+ return `
+ <section id=${attraction.id}>
+  <h3>Attraction</h3>
+  <div>
+   <p>${attraction.name}</p>
+   <div id="attractionDetail" class="hidden showAttractionDetail">
+    <p>${attraction.city}, ${attraction.state}</p>
+    <p>${attraction.description}</p>
+    <p>Restrooms: &#128701 - ${attraction.ameneties.restrooms ? "&#128516" : "&#128577" }</p>
+    <p>Souvenirs: &#128717 - ${attraction.ameneties.souvenirs ? "&#128516" : "&#128577" }</p>
+   </div>
+   <button id="attraction__button">Details</button>
+  </div>
+ </section>
+ `
+} // Attraction
+
+
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "attraction__button") {
 
@@ -10,19 +29,3 @@ eventHub.addEventListener("click", clickEvent => {
         buttonText.innerHTML = buttonText.innerHTML === "Hide" ? "Details" : "Hide"
     }
 })
-
-export const Attraction = (attraction) => {
- return `
- <section id=${attraction.id}>
-  <h3>Attraction</h3>
-  <div>
-   <p>${attraction.name}</p>
-   <div id="attractionDetail" class="hidden showAttractionDetail">
-    <p>${attraction.city}, ${attraction.state}</p>
-    <p>${attraction.description}</p>
-   </div>
-   <button id="attraction__button">Details</button>
-  </div>
- </section>
- `
-} // Attraction
