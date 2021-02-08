785b7adedc67d12f0d9a06523d25ef1e3e3a9dfd
Dan Ross <daniellross@gmail.com>
2021-02-08 10:37:05 -0600

    moved parkDetailHTML to Park.js, added if statement for cost exception

--- scripts/parks/ParkPreview.js
+++ scripts/parks/ParkPreview.js
@@ -1,5 +1,6 @@
 import { useParks } from './ParkProvider.js'
 import { WeatherList } from '../weather/WeatherList.js'
+import { parkHTML } from './Park.js'
 
 const eventHub = document.querySelector('.mainContainer')
 
@@ -19,43 +20,7 @@ eventHub.addEventListener("parkSelected", parkPreviewEvent => {
 
 // previewPark generates html for the preview card
 const previewPark = (park, targetHTML) => {
-    let parkActivitiesHTMLString = ''
-
-    // iterate of array of park activities and generate html to append to dom
-    if (park.activities.length === 0) {
-        parkActivitiesHTMLString = "No Activites Available"
-    } else {
-        for (const activity of park.activities) {
-            parkActivitiesHTMLString += `
-            <li class="park__activity">${activity.name}</li>`
-        }
-    }
-
-    // add html to dom for the preview card
-    targetHTML.innerHTML = `
-    <h3>Park Selected:</h3>
-    <div class="parkPreview__name">${park.fullName}</div>
-    <div class="parkWeather"></div>
-    
-    <div id="parkDetail" class="hidden">
-        <div class="park__info">
-            <div class="park__state"><b>State: </b>${park.states}</div>
-            <div class="park__fee"><b>Entrance Fee: </b>$${park.entranceFees[0].cost}</div>
-            <div class="activityTitle"><b>Activities: </b></div>
-            <div class="park__activitiesContainer">
-                <ul class="park_activitiesList">${parkActivitiesHTMLString}</ul>
-            </div>
-        </div>
-        <div class="park__imageContainer">
-            <img src="${park.images[0].url}" alt="${park.images[0].altText}" class="park__image">
-            <div class="park__imageDescription"><b>Photo description: </b>${park.images[0].caption}</div>
-        </div>
-
-        
-    </div>
-    <button id="parkDetail__Button">Park Details</button>
-    `
-
+    parkHTML(park, targetHTML)
     WeatherList(park)
 }
 
