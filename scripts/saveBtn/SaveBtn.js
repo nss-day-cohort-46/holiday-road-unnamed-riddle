92e7c026d8c481a66c3d44dcee8178aedd54e9b4
CheoR <cheo.roman+github@gmail.com>
2021-02-08 12:57:15 -0600

    fix - check if user selected all three options

--- scripts/saveBtn/SaveBtn.js
+++ scripts/saveBtn/SaveBtn.js
@@ -1,6 +1,5 @@
 const targetElement = document.querySelector(".forSaveBtn")
 const eventHub = document.querySelector(".mainContainer")
-
 /*
  Rendering Save button to .mainContainer causes parkSelector to not load.
  Possibly due to lag?
@@ -9,33 +8,166 @@ const eventHub = document.querySelector(".mainContainer")
 
 export const Save = () => {
 
-
  return `
-  <button class="saveBtn">Save</button>
+  <button class="saveBtn" type="submit" style="background-color:red;text-align:center;" disable>Save</button>
  `
 }
 
 targetElement.innerHTML += `${Save()}`
 
-// eventHub.addEventListener("parkSelected", changeEvent => {
-//  console.log("park selected")
-//  console.log(changeEvent.detail.chosenParkName)
-//  // when you have all three values
-//  // save
-// })
 
 eventHub.addEventListener("change", changeEvent => {
- const parkSelector = document.querySelector("#parkSelect")
- const park = parkSelector.options[parkSelector.selectedIndex].text
 
- const eaterySelector = document.querySelector("#eateryDropdown")
- const eatery = eaterySelector.options[eaterySelector.selectedIndex].text
 
- const attractionSelector = document.querySelector("#attraction-select")
- const attraction = attractionSelector.options[attractionSelector.selectedIndex].text
 
- console.log("secltor")
- console.log(changeEvent.target.)
+ const _attractionsSelector = document.querySelector("#attraction-select")
+ const _eateriesSelector = document.querySelector("#eateryDropdown")
+ const _parksSelector = document.querySelector("#parkSelect")
+ const saveBtn = document.querySelector(".saveBtn")
 
- // console.log(parks)
+
+ // only allow save if all three selectors have chosen and revert if one is changed back to default
+  if(_parksSelector.value !== "0" && _eateriesSelector.value !== "0" && _attractionsSelector.value !== "0") {
+   const attractionName = _attractionsSelector.options[_attractionsSelector.selectedIndex].innerHTML
+   const eateryName = _eateriesSelector.options[_eateriesSelector.selectedIndex].innerHTML
+   const parkName = _parksSelector.options[_parksSelector.selectedIndex].innerHTML // [_parksSelector.value].innerHTML
+
+   saveBtn.innerHTML = `
+    <button class="saveBtn" type="submit" style="background-color:green;text-align:center;" >Save</button>
+   `
+
+   } else {
+    saveBtn.innerHTML = `
+    <button class="saveBtn" type="submit" style="background-color:red;text-align:center;" disable>Save</button>
+    `
+  }
+
 })
+
+
+
+// import { Attraction } from "./Attraction.js"
+// /*
+//  Bizarrieres
+// */
+
+// let _attractions = []
+// const _url = "http://holidayroad.nss.team/bizarreries"
+// const eventHub = document.querySelector(".mainContainer")
+
+
+// export const useAttractions = () => _attractions.slice()
+
+
+// export const getAttractions = () => {
+//   return fetch(_url)
+//   .then(response => response.json())
+//   .then(parsedAttractions => {
+//    _attractions = parsedAttractions
+//   })
+// } // getAttractions
+
+
+// export const LoadAttractions = () => {
+//  getAttractions()
+//   .then(() => { 
+//    const parsedAttractions = useAttractions()
+//    _render(parsedAttractions)
+//   })
+// } // LoadAttractions
+
+
+// const _render = (attractionCollection) => {
+//   const targetElement = document.querySelector(".attractionSelect")
+//   let attractions = attractionCollection.map((attraction) => `<option value=${attraction.id}>${attraction.name}</option>`).join("")
+
+//   targetElement.innerHTML += `
+//     <label for="attraction-select">Bizzararies:</label>
+//     <select name="attractions" id="attraction-select" class="selectAttraction">
+//       <option value="0" >Select an attraction</option>
+//       ${attractions}
+//     </select>
+//     <div class="attractionPreview"></div>
+//   `
+// } // _render
+
+
+//   eventHub.addEventListener("change", changeEvent => {
+
+//     if(changeEvent.target.value !== "0" && changeEvent.target.id === "attraction-select") {
+//       const customEvent = new CustomEvent("attractionsLoaded", {
+//         detail: {
+//           attractionsLoaded: true,
+//           attractionId: parseInt(changeEvent.target.value)
+//         }
+//       })
+
+//       eventHub.dispatchEvent(customEvent)
+
+//     } // if
+
+//     if(changeEvent.target.value === "0" && changeEvent.target.id === "attraction-select") {
+//       const customEvent = new CustomEvent("attractionsLoaded", {
+//         detail: {
+//           attractionsLoaded: false,
+//           attractionId: parseInt(changeEvent.target.value)
+//         }
+//       })
+//       eventHub.dispatchEvent(customEvent)
+//     } // if
+//   }) // _eventHub
+
+
+// // eventHub.addEventListener ("attractionsLoaded", changeEvent => {
+
+// //   const targetElement = document.querySelector(".attractionPreview")
+
+// //   if(changeEvent.detail.attractionsLoaded) {
+// //     const attractionId = changeEvent.detail.attractionId
+// //     const attraction = useAttractions().find((attraction) => attraction.id === attractionId)
+// //     targetElement.innerHTML = `${Attraction(attraction)}`
+// //   } else {
+// //     targetElement.innerHTML = ``
+// //   }
+// // }) // eventHub
+
+
+// // index.html
+
+// //         <div class="submitBtn">
+// //             <button type="submit" style="background-color:red;text-align:center;" disable>Submit</button>
+// //         </div>
+        
+
+// // Itinery.js
+
+// // const submitElement = document.querySelector(".submitBtn")
+
+// // eventHub.addEventListener("change", changeEvent => {
+
+// //  const _attractionsSelector = document.querySelector("#attraction-select")
+// //  const _eateriesSelector = document.querySelector("#eateryDropdown")
+// //  const _parksSelector = document.querySelector("#parkSelect")
+
+// //   if(_parksSelector.value !== "0" && _eateriesSelector.value !== "0" && _attractionsSelector.value !== "0") {
+// //     const attractionName = _attractionsSelector.options[_attractionsSelector.selectedIndex].innerHTML
+// //     const eateryName = _eateriesSelector.options[_eateriesSelector.selectedIndex].innerHTML
+// //     const parkName = _parksSelector.options[_parksSelector.selectedIndex].innerHTML // [_parksSelector.value].innerHTML
+
+// //   targetElement.innerHTML = `
+// //     <li>${parkName}</li>
+// //     <li>${eateryName}</li>
+// //     <li>${attractionName}</li>
+// //   `
+
+// //  submitElement.innerHTML = `
+// //     <button type="submit" style="background-color:green;text-align:center;">Submit</button>
+// //  `
+
+// //   } else {
+// //   targetElement.innerHTML = ``
+// //   submitElement.innerHTML = `
+// //     <button type="submit" style="background-color:red;text-align:center;" disable>Submit</button>
+// //   `
+// //   }
+// // })
