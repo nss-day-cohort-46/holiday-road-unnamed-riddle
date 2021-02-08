4b0ab9244cfed8bfaeb855bc15e81d4005eb370b
daprieu <daprieu@gmail.com>
2021-02-07 21:50:21 -0600

    missing a closing div element

--- scripts/weather/Weather.js
+++ scripts/weather/Weather.js
@@ -4,7 +4,7 @@ export const Weather = (weatherObj) => {
     return `
     <section class="weather">
         <div class="day">${months[new Date(weatherObj.dt * 1000).getMonth('en-US')]} ${new Date(weatherObj.dt * 1000).getDate('en-US')}
-        <div><img class="wicon" src="http://openweathermap.org/img/w/${weatherObj.weather[0].icon}.png" alt=""
+        <div><img class="wicon" src="http://openweathermap.org/img/w/${weatherObj.weather[0].icon}.png" alt=""</div>
         <div class="weatherDetail"> ${weatherObj.weather[0].description}</div>
         <div class="temp max">${weatherObj.temp.max}°F</div>
         <div class="temp min">${weatherObj.temp.min}°F</div>
