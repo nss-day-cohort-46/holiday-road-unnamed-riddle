95cb35e606d3f9901410e0aed433a527b51fece2
samj0516 <samj0516@gmail.com>
2021-02-08 16:07:18 -0600

    this branch is broken, save css

--- scripts/attractions/AttractionPreview.js
+++ scripts/attractions/AttractionPreview.js
@@ -1,9 +1,19 @@
-import { Attracton } from "./Attraction.js"
+// import { Attraction } from "./Attraction.js"
 
-const eventHub = document.querySelector('.mainContainer')
-const contentTarget = document.querySelector('.attractionPreview')
+// const eventHub = document.querySelector('.mainContainer')
+// const targetElement = document.querySelector('.attractionPreview')
 
+// eventHub.addEventListener ("attractionsLoaded", changeEvent => {
+//   console.log('in evenhubt')
+//   if(changeEvent.detail.attractionsLoaded) {
+//     console.log("in attracions loaded")
+//     // const targetElement = document.querySelector(".attractionPreview")
+//     const attractionId = changeEvent.detail.attractionId
+//     const attraction = useAttractions().find((attraction) => attraction.id === attractionId)
 
+//     targetElement.innerHTML = !attractionId ? `` : `${Attraction(attraction)}`
+//   } 
+// })
 // eventHub.addEventListener ("attractionsLoaded", changeEvent => {
 //   if(changeEvent.detail.attractionsLoaded) {
 //     const attractionId = changeEvent.detail.attractionId
@@ -14,12 +24,12 @@ const contentTarget = document.querySelector('.attractionPreview')
 //   } 
 // })
 
-eventHub.addEventListener ("attractionsLoaded", changeEvent => {
-  if(changeEvent.detail.attractionsLoaded) {
-    const attractionId = changeEvent.detail.attractionId
-    const targetElement = document.querySelector(".attractionPreview")
-    const attraction = useAttractions().find((attraction) => attraction.id === attractionId)
+// eventHub.addEventListener ("attractionsLoaded", changeEvent => {
+//   if(changeEvent.detail.attractionsLoaded) {
+//     const attractionId = changeEvent.detail.attractionId
+//     const targetElement = document.querySelector(".attractionPreview")
+//     const attraction = useAttractions().find((attraction) => attraction.id === attractionId)
     
-    targetElement.innerHTML = `${Attraction(attraction)}`
-  } 
-})
+//     targetElement.innerHTML = `${Attraction(attraction)}`
+//   } 
+// })
